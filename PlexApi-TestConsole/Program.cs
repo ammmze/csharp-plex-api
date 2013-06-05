501fa7860729a616640fa0c2ce83a6b30866b519
Branden Cash <ammmze@gmail.com>
2013-06-04 22:09:25 -0700

    Beginning of plex api...and learning C#

---
+++ PlexApi-TestConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlexAPI;
+
+namespace Test
+{
+    class Program
+    {
+
+        static MyPlex plex;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from C#.");
+            plex = new MyPlex();
+
+            var command = "";
+            do
+            {
+                Console.WriteLine("Enter a command");
+                command = Console.ReadLine();
+                Console.WriteLine("Running " + command);
+                switch (command)
+                {
+                    case "auth":
+                    case "authenticate":
+                        Authenticate();
+                        break;
+                }
+            } while (command != "quit");
+
+        }
+
+        static void Authenticate()
+        {
+            Console.WriteLine("Username");
+            var username = Console.ReadLine();
+
+            Console.WriteLine("Password");
+            var password = Console.ReadLine();
+
+            var user = plex.authenticate(username, password);
+        }
+
+
+    }
+}
---
+++ PlexApi/MyPlex.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace PlexAPI
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public int id { get; set; }
+        public string thumb { get; set; }
+        public string queueEmail { get; set; }
+        public string queueUid { get; set; }
+        public string cloudSyncDevice { get; set; }
+        public string authenticationToken { get; set; }
+    }
+    public class MyPlex
+    {
+        const string BaseUrl = "https://my.plexapp.com";
+
+        protected RestClient GetRestClient(String baseurl = BaseUrl)
+        {
+            var client = new RestClient();
+            client.BaseUrl = BaseUrl;
+            return client;
+        }
+
+        protected RestRequest AddPlexHeaders(RestRequest request)
+        {
+            request.AddHeader("X-Plex-Platform", "Windows");
+            request.AddHeader("X-Plex-Platform-Version", "7");
+            request.AddHeader("X-Plex-Provides", "player");
+            request.AddHeader("X-Plex-Client-Identifier", "AB6CCCC7-5CF5-4523-826A-B969E0FFD8A0");
+            request.AddHeader("X-Plex-Product", "PlexWMC");
+            request.AddHeader("X-Plex-Version", "0");
+
+            return request;
+        }
+
+        public T Execute<T>(RestRequest request, RestClient client) where T : new()
+        {
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+            return response.Data;
+        }
+
+        public T Execute<T>(RestRequest request, User user) where T : new()
+        {
+            var client = GetRestClient();
+
+            request = AddPlexHeaders(request);
+
+            request.AddParameter("auth_token", user.authenticationToken);
+
+            return Execute<T>(request, client);
+        }
+
+        public T Execute<T>(RestRequest request, String username, String password) where T : new()
+        {
+
+            var client = GetRestClient();
+
+            request = AddPlexHeaders(request);
+            
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            return Execute<T>(request, client);
+        }
+
+        public User authenticate(string username, string password)
+        {
+            var client = new RestClient();
+            client.BaseUrl = BaseUrl + "/users/sign_in.xml";
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest(Method.POST);
+            request.Resource = "users/sign_in.xml";
+            request.RootElement = "user";
+
+            var user = Execute<User>(request, username, password);
+
+            return user;
+        }
+
+    }
+}
